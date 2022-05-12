46c3c6cb30f819d733d47b6860d0df6a87208255
Fredd89 <ficofedebla@gmail.com>
2022-05-12 11:51:51 +0200

    Added multicast logic.

--- UDPTime/src/udpTime/Client.java
+++ UDPTime/src/udpTime/Client.java
@@ -4,40 +4,52 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Client {
 	
 	private final int port = 2000;
-	InetAddress sAddress;
-	DatagramSocket dSocket;
+	InetAddress mcastaddr;
+	InetSocketAddress group;
+	NetworkInterface netIf;
+	MulticastSocket mSocket;
 	DatagramPacket inPacket;
 	DatagramPacket outPacket;
-	String message = "RICHIESTA DATA E ORA", response;
+	String message = "Hello I am the client.", response;
 	byte[] buffer;
 	
 	public void connection() {
 		try {
-			sAddress = InetAddress.getLocalHost();
+			mcastaddr = InetAddress.getByName("224.0.0.20");
+			InetSocketAddress group = new InetSocketAddress(mcastaddr, port);
+			netIf = NetworkInterface.getByName("test");
+			mSocket = new MulticastSocket(port);
+			mSocket.joinGroup(group, netIf);
 			System.out.println("Connected.");
 		}
 		catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void comunication() {
 		try {
-			dSocket = new DatagramSocket();
-			outPacket = new DatagramPacket(message.getBytes(), message.length(), sAddress, port);
-			dSocket.send(outPacket);
+			outPacket = new DatagramPacket(message.getBytes(), message.length(), group);
+			mSocket.send(outPacket);
 			buffer = new byte[256];
 			inPacket = new DatagramPacket(buffer, buffer.length);
-			dSocket.receive(inPacket);
+			mSocket.receive(inPacket);
 			response = new String(inPacket.getData(), 0, inPacket.getLength());
-			System.out.println("Data e ora del server: " + response);
-			dSocket.close();
+			System.out.println("SERVER-> " + response);
+			mSocket.leaveGroup(group, netIf);
+			mSocket.close();
 		}
 		catch (SocketException e) {
 			e.printStackTrace();
